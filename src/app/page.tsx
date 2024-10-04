faa0dec946feb9cb0c685be7385090ad0551a9de
Amna Owais Ansari <aoansari9@gmail.com>
2024-10-04 12:42:32 +0500

    Governor Initiative Profile Interface Pract

---
+++ src/app/page.tsx
@@ -0,0 +1,32 @@
+
+//import Header from "../component/Header"
+export default function Home() {
+  return (
+ // <Header/>
+ 
+
+  <div className="HomeContainer">
+    <p id="para"><span>Governor Sindh</span><br />
+Kamran Khan Tessori <br />
+Certified Cloud <br />
+Applied Generative AI <br />
+Engineer (GenEng) <br />
+Earn up to $5,000 / month <br />
+
+Now admissions are open in Hyderabad</p>
+<button id="b"><input type="submit" name="Apply Now" id="" /></button>
+    <img id="pict" src="https://www.governorsindh.com/logo.png" alt="" />
+    <h1>Tuition Free Education Program on Latest Technologies</h1>
+   <img id="pict2" src="https://www.governorsindh.com/_next/static/media/cover.1d863e39.png" alt="" />
+   
+   <div className="Child1"><a href="https://www.governorsindh.com/">Home</a></div>
+   <div className="Child2"><a href="https://www.governorsindh.com/apply/">Apply</a></div>
+   
+   <div className="Child3"><a href="https://www.governorsindh.com/apply/">Jobs</a></div>
+   <div className="Child4"><a href="https://www.governorsindh.com/apply/">Results</a></div>
+  
+  
+  </div>
+
+  )
+}
